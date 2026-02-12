88cf499fc1d9759b5771cd94121836d0a749e17a
Thaoticgood <fu3martina@gmail.com>
2026-02-12 20:03:13 +0700

    feat: experiment with link next component

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-      <h1>Test Layoutttt</h1>
+      <h1>Test Layouttttffffff</h1>
         {children}
       </body>
     </html>
